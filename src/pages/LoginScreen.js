e86f280c78a03214768d76807e61d0b1886760e6
taha shahzad <taha.shahzad.0728@gmail.com>
2018-07-23 22:08:49 +0500

    added voting screen -working

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
-import {db} from "../firebase-init";
+import { db } from "../firebase-init";
+import { Redirect } from "react-router-dom";
+
 import { Form, Icon, Input, Button, Checkbox } from "antd";
 import "./LoginScreen.css";
 
@@ -7,31 +9,65 @@ import icon from "../icon.png";
 const FormItem = Form.Item;
 
 class LoginScreen extends React.Component {
+  state = {
+    loggedIn: false
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields(async (err, values) => {
       if (!err) {
-        const {email, password} = values;
-        const {search} = this.props.location;
-        
-        const campaignId = search.substring(search.indexOf("=")+1);
+        const { email, password } = values;
+        const { search } = this.props.location;
 
-       const campaignReference = await db.collection('campaigns').doc(campaignId);
-        const campaign = await campaignReference.get();
-        console.log(campaign.data());
+        const campaignId = search.substring(search.indexOf("=") + 1);
+        if (campaignId) {
+          const campaignReference = await db
+            .collection("campaigns")
+            .doc(campaignId);
+          const campaign = await campaignReference.get();
+          const { members } = campaign.data();
 
+          const loggedInMember = members.filter(member => {
+            if (member.email === email && member.password === password) {
+              return member;
+            }
+          });
+
+          if (loggedInMember.length === 1) {
+            window.campaign = {
+              cid: campaignId,
+              user: loggedInMember[0],
+              data: campaign.data()
+            };
+
+            this.setState(prevState => {
+              return {
+                ...prevState,
+                loggedIn: true
+              };
+            });
+          } else {
+            alert("invalid username and password");
+          }
+        } else {
+          alert("give CID as URL query param");
+        }
       }
     });
   };
 
   render() {
+    if (this.state.loggedIn) {
+      return <Redirect to={`/voting-screen`} />;
+    }
     const { getFieldDecorator } = this.props.form;
     return (
       <div className="form-wrapper">
         <style>{"body { background-color: #ffeaa7; }"}</style>
         <div className="form-container">
           <div className="picture-container">
-            <img src={icon} width={`auto`} height={`100%`}/>
+            <img src={icon} width={`auto`} height={`100%`} />
           </div>
           <div>
             <Form onSubmit={this.handleSubmit} className="login-form">
@@ -69,7 +105,6 @@ class LoginScreen extends React.Component {
                   className="login-screen-button"
                   type="primary"
                   htmlType="submit"
-                 
                 >
                   Log in
                 </Button>
--- src/pages/VotingScreen.js
+++ src/pages/VotingScreen.js
@@ -1,13 +1,62 @@
+import { Card, Col, Row, Button} from "antd";
+import { Redirect } from "react-router-dom";
 import React from "react";
 
 class votingScreen extends React.Component {
-    render(){
+  constructor(props) {
+    super(props);
+    this.renderMember = this.renderMember.bind(this);
+    this.handleVote = this.handleVote.bind(this);
+    this.state = {
+        voters: null
+      };
+  }
+  
 
-        return (
-            <div> 
+  componentDidMount() {
+    const { campaign } = window;
+    if(campaign) {
+        const voters = campaign.data.members.filter(member => {
+            return member.voter && member;
+          });
+          
+          const voterComponents = voters.map((voter, index) => {
+            return this.renderMember(voter,index)
+          });
+      
+          console.log(voterComponents);
+          this.setState(prevState => ({
+            ...prevState,
+            voters: voterComponents
+          }));
 
-            </div>
-        );
-    };
-};
+
+    }    
+  }
+  handleVote(e) {
+    console.log(e.target.getAttribute("email"));
+  }
+  renderMember(voter, key) {
+    
+    return (
+      <Col key={key} span={12}>
+        <Card title={voter.title} bordered={true}>
+          <Button onClick={this.handleVote} email={voter.email}>Vote</Button>
+        </Card>
+      </Col>
+    );
+  }
+  render() {
+    if (!window.campaign) {
+      return <Redirect to="/login" />;
+    }
+    return (
+      <div>
+        <div style={{ background: "#ECECEC", padding: "20px" }}>
+          <Row gutter={24}>{this.state.voters ? this.state.voters : null}</Row>
+        </div>
+      </div>
+    );
+  }
+}
 export default votingScreen;
